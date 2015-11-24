50801768e82021414163433dcb6a7fb799b59976
Jure Slak <jure.slak@gmail.com>
2015-11-24 22:59:23 +0100

    Heading, wording.

--- mnenja.php
+++ mnenja.php
@@ -4,7 +4,7 @@
   <h1>Študentska mnenja</h1>
 </div>
 
-<p>V postopku izvolitve so naslednji pedagoški delavci:</p>
+<h2>V postopku izvolitve so naslednji pedagoški delavci:</h2>
 <!-- <p class="smallfont">Trenutno ni v izvolitvi noben pedagoški delavec.</p> -->
 <ul>
   <li>Kos</li>
@@ -23,11 +23,11 @@
 mnenje, ki se upošteva pri kriteriju pedagoške usposobljenosti, kar je pogoj za
 izvolitev kandidata v naziv.</p>
 <p>Če imaš kakršno koli mnenje o zgornjih delavcih, pozitivno ali negativno, ga
-lahko kadarkoli posreduješ na naslov Sveta <a href="mailto:studentski.svet@fmf.uni-lj.si">
+lahko kadarkoli posreduješ na naslov <a href="mailto:studentski.svet@fmf.uni-lj.si">
   studentski.svet@fmf.uni-lj.si</a> ali pa oddaš v predalček Sveta na recepciji
 Oddelka za fiziko. Tvoje mnenje bo obravnavano anonimno!</p>
 
-<p>V letošnjem šolskem letu je SŠ FMF izdal mnenja naslednjim pedagoškim delavcem:</p>
+<h2>V letošnjem šolskem letu je SŠ FMF izdal mnenja naslednjim pedagoškim delavcem:</h2>
 <ul>
   <li><a href='/dokumenti/mnenja/raic_martin.pdf'>Martin Raič</a></li>
   <li><a href='/dokumenti/mnenja/jezernik_urban.pdf'>Urban Jezernik</a></li>
