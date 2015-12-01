7d712fdaf1852a9aba84e05ddeedbbe70010d9a5
Jure Slak <jure.slak@gmail.com>
2015-12-01 15:19:10 +0100

    Update broken links.

--- legal.php
+++ legal.php
@@ -18,20 +18,20 @@
 
 <h2>Temeljni akti Univerze in ostali pravilniki</h2>
 <ul>
-  <li><a href="http://www.uni-lj.si/o_univerzi_v_ljubljani/predpisi_statut_ul_in_pravilniki/statut_univerze_v_ljubljani.aspx" target="_blank">
+  <li><a href="https://www.uni-lj.si/o_univerzi_v_ljubljani/organizacija__pravilniki_in_porocila/predpisi_statut_ul_in_pravilniki/2013070915432663/" target="_blank">
     Statut univerze</a></li>
   <li><a href="http://www.uradni-list.si/1/content?id=108446" target="_blank">
     Zakon o visokem šolstvu</a></li>
-  <li><a href="http://www.uni-lj.si/o_univerzi_v_ljubljani/statut_in_pravilniki/izmenjava_studentov.aspx" target="_blank">
+  <li><a href="https://www.uni-lj.si/o_univerzi_v_ljubljani/organizacija__pravilniki_in_porocila/predpisi_statut_ul_in_pravilniki/2013071216271849/" target="_blank">
     Pravilnik o izmenjavah študentov med članicami UL</a></li>
-  <li><a href="http://www.uni-lj.si/o_univerzi_v_ljubljani/statut_in_pravilniki/pravilnik_o_disciplinski_odgovornosti.aspx" target="_blank">
+  <li><a href="https://www.uni-lj.si/o_univerzi_v_ljubljani/organizacija__pravilniki_in_porocila/predpisi_statut_ul_in_pravilniki/2013071214420651/" target="_blank">
     Pravilnik o disciplinski odgovornosti študentov Univerze v Ljubljani</a></li>
-  <li><a href="http://www.uni-lj.si/o_univerzi_v_ljubljani/statut_in_pravilniki/pravilnik_o_studentski_anketi.aspx" target="_blank">
+  <li><a href="https://www.uni-lj.si/o_univerzi_v_ljubljani/organizacija__pravilniki_in_porocila/predpisi_statut_ul_in_pravilniki/2014092609210542/" target="_blank">
     Pravilnik o izvajanju študentskih anket</a></li>
-  <li><a href="http://www.uni-lj.si/o_univerzi_v_ljubljani/statut_in_pravilniki/pravilnik_o_podeljevanju_presernovih_nagrad_studentom.aspx" target="_blank">
+  <li><a href="https://www.uni-lj.si/o_univerzi_v_ljubljani/organizacija__pravilniki_in_porocila/predpisi_statut_ul_in_pravilniki/2013071216390406/" target="_blank">
     Pravilnik o podeljevanju študentskih Prešernovih nagrad</a></li>
-  <li><a href="http://www.uni-lj.si/o_univerzi_v_ljubljani/statut_in_pravilniki/pravilnik_o_studentski_izkaznici.aspx" target="_blank">
-    Pravilnik o študentski izkaznici</a></li>
+  <li><a href=https://www.uni-lj.si/o_univerzi_v_ljubljani/organizacija__pravilniki_in_porocila/predpisi_statut_ul_in_pravilniki/" target="_blank">
+    Ostali pravilniki</a></li>
 </ul>
 
 <h2>Zapisniki s sej sveta</h2>
