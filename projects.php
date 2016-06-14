5e77b88ec04d6c2fdc5ccf9b8fb0c8107e6fcee9
Jure Slak <jure.slak@gmail.com>
2016-06-14 20:22:14 +0200

    Fixed wording.

--- projects.php
+++ projects.php
@@ -6,18 +6,18 @@
 
 <h2>Stalne dejavnosti</h2>
 <ul>
-  <li>priprava študentskih mnenj za izvolitve v profesorske nazive</li>
+  <li>priprava študentskih mnenj za izvolitve v učiteljske nazive</li>
   <li>sodelovanje pri opravljanju študentskih anket</li>
   <li>zastopanje študentskih interesov v organih FMF in Univerze v Ljubljani</li>
   <li>reševanje študentskih problemov (neprijazni profesorji, preobremenjenost, pitna voda &hellip;)</li>
-  <li>skupaj s Študentsko organizacijo FMF organiziramo Mafijski piknik, strokovno ekskurzijo, dobrodelni teden &hellip; </li>
+  <li>skupaj s ŠO FMF organiziramo Mafijski piknik, pi dan, novoletno stojnico, &hellip; </li>
 </ul>
 <h2>Zunanji projekti pri katerih sodelujemo</h2>
 <ul>
-  <li>bolonjska reforma</li>
-  <li>uvedba elektronskih prijav na izpite</li>
   <li>tutorstvo</li>
   <li>karierni centri</li>
+  <li>sodelovanje s podjetji</li>
+  <li>športne aktivnosti</li>
 </ul>
 
 <?php require_once('footer.php'); ?>
