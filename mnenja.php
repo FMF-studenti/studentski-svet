76cb922c9d6c1be9a2952be8366de6f51a97239e
Jure Slak <jure.slak@gmail.com>
2016-06-14 18:11:37 +0200

    Dodal rešena mnenja.

--- mnenja.php
+++ mnenja.php
@@ -13,11 +13,7 @@ bolj natančno. S tem imate zelo močen vpliv na kvaliteto predavanj!
 <h2>V postopku izvolitve so naslednji pedagoški delavci:</h2>
 <!-- <p class="smallfont">Trenutno ni v izvolitvi noben pedagoški delavec.</p> -->
 <ul>
-  <li>Robert Jeraj</li>
-  <li>Dejan Govc</li>
-  <li>Drejc Kopač</li>
-  <li>Simon Čopar</li>
-  <li>Andrej Studen</li>
+  <li>Janez Šter</li>
 </ul>
 <p>Za te pedagoške delavce bo Študentski svet FMF v kratkem izdelal študentsko
 mnenje, ki se upošteva pri kriteriju pedagoške usposobljenosti, kar je pogoj za
@@ -29,40 +25,50 @@ Oddelka za fiziko. Tvoje mnenje bo obravnavano anonimno!</p>
 
 <h2>V letošnjem šolskem letu je SŠ FMF izdal mnenja naslednjim pedagoškim delavcem:</h2>
 <ul>
-   <li>Selena Praprotnik      </li>
-   <li>Maruška Žerjal         </li>
-   <li>Jure Kališnik          </li>
-   <li>Igor Dobovšek          </li>
-   <li>Zoran Arsov            </li>
-   <li>Gašper Žerovnik        </li>
-   <li>Aljaž Zalar            </li>
-   <li>Robert Jeraj           </li>
-   <li>Simon Širca            </li>
-   <li>Barbara Modec          </li>
-   <li>Tomaž Mertelj          </li>
-   <li>Urban Simončič         </li>
-   <li>Matija Lokar           </li>
-   <li>Nejc Košnik            </li>
-   <li>Simon Širca            </li>
-   <li>Davorin Lešnik         </li>
-   <li>Janez Kos              </li>
-   <li>Aleksej Majkič         </li>
-   <li>Sergio Cabellio Justo  </li>
-   <li>Igor Dobovšek          </li>
-   <li>Matjaž Zaveršnik       </li>
-   <li>Bor Plestenjak         </li>
-   <li>Jernej Azarija         </li>
-   <li>Marjetka Conradi       </li>
-   <li>Irena Drevenšek-Olenik </li>
-   <li>Mihael Perman          </li>
-   <li>Andreja Šarlah         </li>
-   <li>Martin Raič            </li>
-   <li>Urban Jezernik         </li>
-   <li>Nik Stopar             </li>
-   <li>Kris Stopar            </li>
-   <li>Riste Škrekovski       </li>
-   <li>Leon Cizelj            </li>
-   <li>Matija Uršič           </li>
+  <li>Dejan Govc</li>
+  <li>Simon Čopar</li>
+  <li>Andrej Studen</li>
+  <li>Drejc Kopač</li>
+  <li>Arjana Žitnik</li>
+  <li>Petra Tomše</li>
+  <li>Martin Klanjšek</li>
+  <li>Robert Jeraj</li>
+  <li>Primož Koželj</li>
+  <li>Barbara Drinovec Drnovšek</li>
+  <li>Selena Praprotnik      </li>
+  <li>Maruška Žerjal         </li>
+  <li>Jure Kališnik          </li>
+  <li>Igor Dobovšek          </li>
+  <li>Zoran Arsov            </li>
+  <li>Gašper Žerovnik        </li>
+  <li>Aljaž Zalar            </li>
+  <li>Robert Jeraj           </li>
+  <li>Simon Širca            </li>
+  <li>Barbara Modec          </li>
+  <li>Tomaž Mertelj          </li>
+  <li>Urban Simončič         </li>
+  <li>Matija Lokar           </li>
+  <li>Nejc Košnik            </li>
+  <li>Simon Širca            </li>
+  <li>Davorin Lešnik         </li>
+  <li>Janez Kos              </li>
+  <li>Aleksej Majkič         </li>
+  <li>Sergio Cabellio Justo  </li>
+  <li>Igor Dobovšek          </li>
+  <li>Matjaž Zaveršnik       </li>
+  <li>Bor Plestenjak         </li>
+  <li>Jernej Azarija         </li>
+  <li>Marjetka Conradi       </li>
+  <li>Irena Drevenšek-Olenik </li>
+  <li>Mihael Perman          </li>
+  <li>Andreja Šarlah         </li>
+  <li>Martin Raič            </li>
+  <li>Urban Jezernik         </li>
+  <li>Nik Stopar             </li>
+  <li>Kris Stopar            </li>
+  <li>Riste Škrekovski       </li>
+  <li>Leon Cizelj            </li>
+  <li>Matija Uršič           </li>
 </ul>
 
 <?php require_once 'footer.php'; ?>
