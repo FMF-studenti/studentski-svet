f691d5c8c17bf84be62ad6e6d2e37eeeeaac8731
Jure Slak <jure.slak@gmail.com>
2016-06-26 22:00:44 +0200

    Dodal stran za Ljubljanski maraton.

---
+++ ljm.php
@@ -0,0 +1,31 @@
+<?php require_once('header.php'); ?>
+
+<div id="title">
+  <h1>Ljubljanski maraton 2016</h1>
+</div>
+
+<h2>Skupinska prijava</h2>
+<p>Skupinska prijava na Ljubljanski maraton je cenejša, zato se FMF-jevci prijavimo skupaj. Za
+prijave letos skrbi ga. Greta Forster, ki je za informacije dosegljiva na <a
+href='mailto:greta.forster@fmf.uni-lj.si'>elektronski naslov</a> ali osebno na recepciji Jadranske
+19. Za več informacij lahko kontaktiraš tudi <a href='mailto:veno.mramor@gmail.com'>Vena Mramorja</a>.</p>
+
+<p>Cena pri skupinski prijavi je 30&euro;/ osebo, kar je ceneje, kot če se se vsak prijavi sam. Če
+imaš svoj čip, potem je cena še za 3&euro; manjša.  Več informacij glede cen, tekov in časovnih
+terminov je na voljo na <a href='http://vw-ljubljanskimaraton.si/' target='_blank'>spletni strani
+Ljubljanskega maratona</a>.
+
+<p> NOVO - RIZIKO ODPOVEDI: S plačilom rizika odpovedi si pridružujete pravico do vračila stroškov
+prijavnine v primeru odpovedi nastopa na maratonu. Riziko odpovedi znaša 5&euro; in ga plačate ob
+prijavi. Brez plačanega rizika odpovedi vračilo v nobenem primeru (tudi ob predložitvi zdravniškega
+potrdila) ne bo mogoče.  </p>
+
+<p>Za prijavo:</p>
+<ul>
+  <li>Izpolni <a href='http://goo.gl/forms/vupaeaVegefgUkqK2'>anketo</a> z osebnimi podatki za prijavo (uporabili jih bomo samo za prijavo).</li>
+  <li>Greti Forster prinesi štartnino na recepcijo Jadranske 19.  Denar lahko pustiš tudi pri katerem drugem vratarju na fiziki, vendar naj bo jasno za kaj gre.</li>
+</ul>
+
+<p><strong>Rok za prijavo na FMF je 12. 8. 2016</strong>.</p>
+<p><strong>Rok za odjavo in vračilo prijavnine s plačanim odpovednim rizikom je 4. 11. 2016, pod pogojem, da štartni paket ni prevzet! </strong></p>
+<?php require_once('footer.php'); ?>
