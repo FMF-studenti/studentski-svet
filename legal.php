2cbba2ea42c097dbe564de487ebb2f07652b6323
Jure Slak <jure.slak@gmail.com>
2016-10-08 21:46:34 +0200

    Typo.

--- legal.php
+++ legal.php
@@ -138,7 +138,7 @@ SVG).</p>
   <li><a href="/dokumenti/plakati/mafijski_piknik_after.pdf">Reklama za after party</a></li>
   <li><a href="/dokumenti/plakati/pi_predstava.pdf">Reklama za predstavo Nika Škrleca o Pi-ju</a></li>
   <li><a href="/dokumenti/plakati/maraton.pdf">Skupinska prijava na Ljubljanski maraton</a>. Pripadajoča <a href="/ljm">stran</a>.</li>
-  <li><a href="/dokumenti/plakati/guzva_referat.pdf">Napotki za mnjšo gnečo v referatu</a>.
+  <li><a href="/dokumenti/plakati/guzva_referat.pdf">Napotki za manjšo gnečo v referatu</a>.
   <li><a href="/dokumenti/plakati/ali_ves_2016_17.pdf">O dejavnostih sveta drugič</a></li>
 </ul>
 
