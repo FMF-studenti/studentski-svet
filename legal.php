0ce47fe833df8a5fdaf5df5731dbc1a41ecd191d
Jure Slak <jure.slak@gmail.com>
2016-12-23 18:28:55 +0100

    Zapisnik 2 seje.

--- legal.php
+++ legal.php
@@ -39,6 +39,7 @@
 <h2>Zapisniki s sej sveta</h2>
 <ul>
   <li>Zapisnik 1. redne (konstitutivne) seje je na voljo <a href="/dokumenti/zapisniki/2016_17/1_seja.pdf">tukaj</a></li>
+  <li>Zapisnik 2. redne seje je na voljo <a href="/dokumenti/zapisniki/2016_17/2_seja.pdf">tukaj</a></li>
 </ul>
 
 <h2 id='logos'>Logotipi sveta</h2>
