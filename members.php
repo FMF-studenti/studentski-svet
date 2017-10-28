15dfdd26fa87b069069ae913ec4965e986f253bf
Jure Slak <jure.slak@gmail.com>
2017-10-28 18:34:07 +0200

    Opis funkcij.

--- members.php
+++ members.php
@@ -24,6 +24,22 @@
 </table>
 
 <h1>Vodstvo sveta</h1>
+<p>
+<strong>Predsednik ŠS FMF</strong> skrbi za nemoteno delovanje Sveta. Vodi seje in dodeljuje tekoče
+naloge drugim članom. Prevzema odgovornost za komunikacijo z vodstvom fakultete
+in predstavlja študentski svet. Običajno pozicija letno alternira med matematiki in
+fiziki. <br>
+<strong>Podpredsednik ŠS FMF</strong> nadomešča predsednika v njegovi odsotnosti in mu pomaga
+pri tekočih zadevah. Običajno je iz nasprotnega oddelka kot predsednik in skrbi za
+zadeve, ki se tičejo njegovega oddelka.<br>
+<strong>Tajnik ŠS FMF</strong> zapisuje dogajanje med sejami in pripravi zapisnik sej Sveta. Skrbi za
+natančen povzetek sej sveta in njihovo objavo ter posredno za dobro obveščenost
+študentov.<br>
+<strong>Predstavnik za stike z RC</strong> skrbi za spletno stran in elektronski naslov Sveta ter ostalo
+informacijsko podporo s pomočjo RC FMF.
+</p>
+
+
 <p><strong>Predsednik</strong> sveta je Jure Slak, <strong>podpredsednik</strong> pa je Miha Srdinšek.
 Njuna namestnika sta Ana Kregar in Klemen Ducman. <strong>Tajnica</strong> sveta
 je Mojca Žilavec, namestnica je Ines Meršak. <strong>Predstavnik za stike z RC</strong> je Jure Slak,
@@ -31,6 +47,9 @@ njegova namestnica je Ines Meršak.</p>
 
 <h1>Predstavniki v organih FMF</h1>
 <h2>Senat FMF</h2>
+<p>Senat je najvišji organ fakultete in ima zaključne odločitve pri tekočih izvolitvah, spremembah
+programov in ostalih tekočih zadevah. ŠS ima v senatu 3 predstavnike. Senat se sestaja mesečno.
+Predstavniki študentov:</p>
 <ul>
   <li>Klemen Ducman, namestnik Miha Srdinšek</li>
   <li>Saša Marolt, namestnica Mojca Žilavec</li>
@@ -38,38 +57,57 @@ njegova namestnica je Ines Meršak.</p>
 </ul>
 
 <h2>UO (upravni odbor) FMF</h2>
+<p> Upravni odbor FMF se odloča o finančnih zadevah fakultete, med drugim o cenah storitev, ki
+vplivajo na študente. Predstavnik študentov:</p>
 <ul>
   <li>Klemen Ducman, namestnik Miha Srdinšek</li>
 </ul>
 
 <h2>ZPS (znanstveno pedagoški svet)</h2>
+<p>Znanstevno pedagoški svet na vsakem oddelku posebej odloča o raziskovalnih in pedagoških
+zadevah, npr. izvolitvah, spremembah programov ipd. Sestaja se mesečno, odločitve se posredujejo
+senatu FMF.  Predstavnika študentov: </p>
 <ul>
   <li>Oddelek za matematiko: Rok Gregorič, namestnica Ana Smerdu</li>
   <li>Oddelek za fiziko: Miha Srdinšek, namestnik Klemen Ducman </li>
 </ul>
 
 <h2>Komisija za študenstka mnenja</h2>
-<p>Imen članov komisije za študentska mnenja po sklepu Študentskega sveta ne bomo javno objavili.</p>
+<p> Komisija za študentska mnenja piše študentska mnenja o profesorjih in asistentih ob izvolitvah s
+pomočjo študentskih anket. Za uspešno izvolitev mora pedagoški delavec dobiti pozitivno študentsko
+mnenje. V primeru negativnega mnenja ŠS z vodstvom fakultete in obravnavanim kandidatom izvede
+sestanek in sprejme ukrepe za njegovo izboljšanje. Imen članov komisije za študentska mnenja po
+sklepu Študentskega sveta ne bomo javno objavili.</p>
 
 <h2>Disciplinska komisija</h2>
+<p>Disciplinska komisija se sestane kadar pride do goljufij pri preverjanju znanja ali pri
+drugih kršitvah pravilnika s strani študentov. Predstavnika:
+</p>
 <ul>
   <li>Klemen Ducman, namestnica Saša Marolt</li>
   <li>Miha Šifrer, namestnica Ana Smerdu</li>
 </ul>
 
 <h2>Komisija za etična vprašanja</h2>
+<p>Komisija za etična vprašanja se sestane v primeru etičnih vprašanj glede raziskovalnega ali
+pedagoškega dela. Predstavnica študentov:</p>
 <ul>
   <li>Saša Marolt, namestnik Miha Srdinšek</li>
 <!--  <li>Mojca Žilavec, namestnica Ana Smerdu</li> -->
 </ul>
 
 <h2>Komisija za kakovost</h2>
+<p> Komisija za kakovost se sestane skupaj z Nacionalno agencijo RS za kakovost v visokem šolstvu
+ob ponovni akreditaciji študijskih programov. Običajno se poleg članov Sveta sestanejo tudi s
+študenti študijskih programov v obravnavi. Predstavniki študentov:</p>
 <ul>
   <li>Miha Šifrer, namestnica Ana Smerdu</li>
   <li>Miha Srdinšek, namestnik Miha Srdinšek</li>
 </ul>
 
 <h2>Komisija za samoevaluacijo</h2>
+<p> Komisija za samoevaluacijo pripravlja notranje poročilo o kakovosti fakultete in njenega
+delovanja. Predstavniki študentov:</p>
 <ul>
   <li>Ana Smerdu, namestnica Ines Meršak</li>
   <li>Marion van Midden, namestnica Saša Marolt</li>
