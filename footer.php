6e3bb695ea4e6f7313a5ea93977b1ad06de027a1
MaticTonin <matic.tonin@student.fmf.uni-lj.si>
2022-01-09 19:05:35 +0100

    update

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 
       <div id="footer">
         <span class="email"><a href="mailto:studentski.svet@fmf.uni-lj.si">studentski.svet@fmf.uni-lj.si</a></span>
+        <a href="default.asp"><img src="/img/fb.jpg" alt="https://www.facebook.com/Studentski.svet.FMF" style="width:42px;height:42px;"></a>
         <span class="author">&copy; Študentski svet Fakultete za matematiko in fiziko</span>
         <br class="clear" />
       </div>
